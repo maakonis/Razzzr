f1444af9e32f1064cdd435f0ddf1165523edaf03
Jerome Wynne <J.J.Wynne47@gmail.com>
2017-08-03 13:42:53 +0100

    Working build of rig

--- Proposal/Arduino/FSR_NDOF_Collection_datalogger/FSR_NDOF_Collection_datalogger.ino
+++ Proposal/Arduino/FSR_NDOF_Collection_datalogger/FSR_NDOF_Collection_datalogger.ino
@@ -1,37 +1,42 @@
+#include <imumaths.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
+#include <stdio.h>
 #include <Wire.h>
 #include <SPI.h>
 #include <SD.h>
 
 
 /* > General Declarations < */
-const int time_step = 50;      // samping period of sensors
-const int datapts = 400;       // number of data points collected before stored in the datalogger
-int arr_counter = 0;           // counts the datapoints and acts as ID (1,2,3...) in the .csv file
 const bool PRINT_MEASUREMENTS = false;
-const bool WRITE_DEMO_FILE    = true;
+const bool WRITE_DEMO_FILE    = false;
+const int sampling_period = 50;      // samping period of sensors
+int time_step             = 0;
 
 /* > Datalogger Declarations < */
+const String HEADER = "time, th_x, th_y, th_z, acc_x, acc_y, acc_z, mag_x, mag_y, mag_z, force, temperature";
+const String CSV_name = "0803_001.csv";
 const int CSpin = 4;
 File dataFile;
-String CSVName = "0803001.csv";
-Adafruit_BNO055 bno = Adafruit_BNO055(55); // What does the 55 do?
 
 /* > Orientation Sensor Declarations < */
+Adafruit_BNO055 bno = Adafruit_BNO055(55); // What does the 55 do?
+imu::Vector<3> orientation;
+imu::Vector<3> acceleration;
+imu::Vector<3> magnetic_field;
+const int ACCELERATION_SF = 100;
+int temperature;
 
 /* > Force Sensor Declarations < */
 int fsrPin = 0;     // the FSR and 10K pulldown are connected to a0
 int fsrReading;
-int fsrRemap;
+int force;
 
 
 void setup(void) {
-
-  delay(5000);
   Serial.begin(9600);
  
-  /* 1. Attempt to initialize the SD card. */
+  /* 1. Initialize the SD card. */
   Serial.print("Initializing SD card... ");
   pinMode(CSpin, OUTPUT);
  
@@ -39,13 +44,13 @@ void setup(void) {
     Serial.println("Card failed, or not present.");
     return;
   }
-  
-  Serial.println("Card initialized.");  
-  delay(100); //wdt safety?
+  else {
+    Serial.println("Card initialized.");  
+  }
   
   if (WRITE_DEMO_FILE) {
     Serial.print("Writing demo file to card... ");
-    File dataFile = SD.open("demofile.txt", FILE_WRITE);
+    File dataFile = SD.open("DEMOFILE.txt", FILE_WRITE);
     
     if (dataFile) {
       dataFile.println("Hello, world!");
@@ -53,116 +58,86 @@ void setup(void) {
       Serial.println("Demo write successful.");
     }
     else {
-      Serial.print("Demo write unsuccessful.");
+      Serial.print("Demo write failed..");
     }
   }
+
   /* 2. Initialize the orientation sensor. */
   Serial.print("Initializing orientation sensor... ");
-  
   if(!bno.begin()) {
     Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
     while(1);
   }
-  Serial.println("Sensor initialized.");
-  
-  delay(1000); // Why?
-    
   bno.setExtCrystalUse(true); // Synchronize the collection of data using the Feather's clock, not the one on-board the BNO.
+  Serial.println("Sensor initialized.");
+
+  /* 3. Write a header to the SD card file. */
+  writeHeader(CSV_name, HEADER);
 }
 
 
 void loop() {
-   String euler_x = "0";
-   String euler_y = "0";
-   String euler_z = "0";
-   String accel_x = "0";
-   String accel_y = "0";
-   String accel_z = "0";
-   String arr_string = "0";
-   String fsr_touch = "0";
-   arr_counter = 1;
+  /* 1. Collect BNO055 sensor data. */
+  sensors_event_t event; // Associate memory with an event object
+  bno.getEvent(&event);  // Send the orientation sensor's data to the in-memory address of the event object
 
-   /* 1. Collect <datapts> measurements and store them in strings. */
-    while (arr_counter < datapts) {
-      
-      /* 1.1 Collect orientation and acceleration data. */
-      sensors_event_t event; // Associate memory with an event object
-      bno.getEvent(&event);  // Send the orientation sensor's data to the in-memory address of the event object
-      
-      euler_x += "," +  String(event.orientation.x); // azimuth, pitch, roll?;
-      euler_y += "," +  String(event.orientation.y);
-      euler_z += "," +  String(event.orientation.z);
-
-      accel_x += "," + String(event.acceleration.x);
-      accel_y += "," + String(event.acceleration.y);
-      accel_z += "," + String(event.acceleration.z);
-
-      // I think we can use event.orientation.v to retrieve all components simultaneously as a list
-      
-      /* 1.2. Collect force data. */
-      fsrReading = analogRead(fsrPin);
-      fsrRemap = map(fsrReading, 0, 1023, 0, 100);
-      fsr_touch += "," + String(fsrRemap);
+  orientation    = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+  acceleration   = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+  magnetic_field = bno.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
+  temperature = bno.getTemp();
   
-      /* 1.3. Print readings to the monitor. */
-      if (PRINT_MEASUREMENTS) {
-        Serial.print("Analog reading = ");
-        Serial.print(fsrReading);
-     
-        if (fsrReading < 10) {
-          Serial.println(" - No pressure");
-        } else if (fsrReading < 200) {
-          Serial.println(" - Light touch");
-        } else if (fsrReading < 500) {
-          Serial.println(" - Light squeeze");
-        } else if (fsrReading < 800) {
-          Serial.println(" - Medium squeeze");
-        } else {
-          Serial.println(" - Big squeeze");
-        }
-        
-        Serial.print("X orientation: ");
-        Serial.print(event.orientation.x);
-        Serial.print("\tY orientation: ");
-        Serial.print(event.orientation.y);
-        Serial.print("\tZ orientation: ");
-        Serial.print(event.orientation.x);
-        Serial.println("");
-      }
-      
-      /* 1.4. Increment the datapoint counter. */
-      arr_counter += 1;
-      arr_string  += "," + String(arr_counter);
-      
-      delay(time_step);
-    }
-
+  /* 2. Collect force data. */
+  fsrReading  = analogRead(fsrPin);
+  force       = map(fsrReading, 0, 1023, 0, 100);
   
-  /* 2. Write the strings to the datalogger. */
-  saveData(arr_string, CSVName);
-  saveData(euler_x, CSVName);
-  saveData(euler_y, CSVName);
-  saveData(euler_z, CSVName);
-  saveData(accel_x, CSVName);
-  saveData(accel_y, CSVName);
-  saveData(accel_z, CSVName);
-  saveData(fsr_touch, CSVName);
+  /* 3. Write measurements to the SD card. */
+  writeData(CSV_name,
+            12,
+            (int) time_step,
+            (int) orientation.x(),
+            (int) orientation.y(),
+            (int) orientation.z(),
+            (int) (ACCELERATION_SF * acceleration.x()),
+            (int) (ACCELERATION_SF * acceleration.y()),
+            (int) (ACCELERATION_SF * acceleration.z()),
+            (int) magnetic_field.x(),
+            (int) magnetic_field.y(),
+            (int) magnetic_field.z(),
+            (int) force,
+            (int) temperature
+            );
 
-  Serial.println("Step index: " + arr_string);
-  Serial.println("Orientation w.r.t. x-axis:" + euler_x);
-  
-  delay(5000); // Why?      
-  }
+  time_step  += 1;
+  delay(sampling_period);
+}
       
 
-void saveData(String dataString, String filename){
+void writeData(String filename, int n_args, ...){
+  String valString;
+  va_list argList;
   dataFile = SD.open(filename, FILE_WRITE);
   
   if (dataFile){
-    dataFile.println(dataString);
+    va_start(argList, n_args);
+    for (int i = 0; i < n_args; i++) { // Print each argument to a line in the file
+      valString = String(va_arg(argList, int));
+      dataFile.print(valString + ",");
+    }
+    va_end(argList);
+    
+    dataFile.println("");
     dataFile.close();
   }
   else{
-    Serial.println("Error writing to file.");
+    Serial.println("Error writing data to file.");
   }
+}
+
+void writeHeader(String filename, String header){
+  dataFile = SD.open(filename, FILE_WRITE);
+  
+  if (dataFile) { dataFile.println(header); }
+  else{ Serial.println("Error writing header to file."); }
+  
+  dataFile.close();
 }

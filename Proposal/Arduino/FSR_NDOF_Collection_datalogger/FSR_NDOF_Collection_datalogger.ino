6fd2d5864e7e376406f5c45f8b814b47677be661
Jerome Wynne <J.J.Wynne47@gmail.com>
2017-08-03 10:50:58 +0100

    Added write demo, need to find out why writing the data strings to the logger is failing...

--- Proposal/Arduino/FSR_NDOF_Collection_datalogger/FSR_NDOF_Collection_datalogger.ino
+++ Proposal/Arduino/FSR_NDOF_Collection_datalogger/FSR_NDOF_Collection_datalogger.ino
@@ -1,4 +1,3 @@
-#include <utility/imumaths.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <Wire.h>
@@ -8,19 +7,18 @@
 
 /* > General Declarations < */
 const int time_step = 50;      // samping period of sensors
-const int datapts = 400;       //number of data points collected before stored in the datalogger
-int arr_counter = 0;           //counts the datapoints and acts as ID (1,2,3...) in the .csv file
+const int datapts = 400;       // number of data points collected before stored in the datalogger
+int arr_counter = 0;           // counts the datapoints and acts as ID (1,2,3...) in the .csv file
+const bool PRINT_MEASUREMENTS = false;
+const bool WRITE_DEMO_FILE    = true;
 
 /* > Datalogger Declarations < */
 const int CSpin = 4;
 File dataFile;
-String CSVName = "0803_001.csv"
+String CSVName = "0803001.csv";
 Adafruit_BNO055 bno = Adafruit_BNO055(55); // What does the 55 do?
 
 /* > Orientation Sensor Declarations < */
-int X_pos; // ????
-int Y_pos;
-int Z_pos;
 
 /* > Force Sensor Declarations < */
 int fsrPin = 0;     // the FSR and 10K pulldown are connected to a0
@@ -28,15 +26,15 @@ int fsrReading;
 int fsrRemap;
 
 
-void setup(void) 
-{
-  Serial.begin(115200);
-  Serial.flush();
-  
-  Serial.print("Initializing SD card...");
-  pinMode(CSpin, OUTPUT);
+void setup(void) {
 
+  delay(5000);
+  Serial.begin(9600);
+ 
   /* 1. Attempt to initialize the SD card. */
+  Serial.print("Initializing SD card... ");
+  pinMode(CSpin, OUTPUT);
+ 
   if (!SD.begin(CSpin)) {
     Serial.println("Card failed, or not present.");
     return;
@@ -45,23 +43,35 @@ void setup(void)
   Serial.println("Card initialized.");  
   delay(100); //wdt safety?
   
+  if (WRITE_DEMO_FILE) {
+    Serial.print("Writing demo file to card... ");
+    File dataFile = SD.open("demofile.txt", FILE_WRITE);
+    
+    if (dataFile) {
+      dataFile.println("Hello, world!");
+      dataFile.close();
+      Serial.println("Demo write successful.");
+    }
+    else {
+      Serial.print("Demo write unsuccessful.");
+    }
+  }
   /* 2. Initialize the orientation sensor. */
-  Serial.println("Orientation Sensor Test"); Serial.println("");
+  Serial.print("Initializing orientation sensor... ");
   
-  if(!bno.begin())
-  {
+  if(!bno.begin()) {
     Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
     while(1);
   }
+  Serial.println("Sensor initialized.");
   
   delay(1000); // Why?
     
-  bno.setExtCrystalUse(true); // Synchronise the collection of data using the Feather's clock, not the one on-board the BNO.
+  bno.setExtCrystalUse(true); // Synchronize the collection of data using the Feather's clock, not the one on-board the BNO.
 }
 
 
-void loop() 
-{
+void loop() {
    String euler_x = "0";
    String euler_y = "0";
    String euler_z = "0";
@@ -74,21 +84,18 @@ void loop()
 
    /* 1. Collect <datapts> measurements and store them in strings. */
     while (arr_counter < datapts) {
+      
       /* 1.1 Collect orientation and acceleration data. */
       sensors_event_t event; // Associate memory with an event object
       bno.getEvent(&event);  // Send the orientation sensor's data to the in-memory address of the event object
       
-      X_pos    = (int) event.orientation.x; // float?
-      euler_x += "," + String(X_pos);
-      Y_pos    = (int) event.orientation.y; // azimuth, pitch, roll?
-      euler_y += "," +  String(Y_pos);
-      Z_pos    = (int) event.orientation.z;
-      euler_z += "," +  String(Z_pos);
-      
-      imu::Vector<3> accel = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER); // event.acceleration.x etc.?
-      accel_x += "," + String(accel.x());
-      accel_y += "," + String(accel.y());
-      accel_z += "," + String(accel.z());
+      euler_x += "," +  String(event.orientation.x); // azimuth, pitch, roll?;
+      euler_y += "," +  String(event.orientation.y);
+      euler_z += "," +  String(event.orientation.z);
+
+      accel_x += "," + String(event.acceleration.x);
+      accel_y += "," + String(event.acceleration.y);
+      accel_z += "," + String(event.acceleration.z);
 
       // I think we can use event.orientation.v to retrieve all components simultaneously as a list
       
@@ -98,29 +105,31 @@ void loop()
       fsr_touch += "," + String(fsrRemap);
   
       /* 1.3. Print readings to the monitor. */
-      Serial.print("Analog reading = ");
-      Serial.print(fsrReading);
-   
-      if (fsrReading < 10) {
-        Serial.println(" - No pressure");
-      } else if (fsrReading < 200) {
-        Serial.println(" - Light touch");
-      } else if (fsrReading < 500) {
-        Serial.println(" - Light squeeze");
-      } else if (fsrReading < 800) {
-        Serial.println(" - Medium squeeze");
-      } else {
-        Serial.println(" - Big squeeze");
+      if (PRINT_MEASUREMENTS) {
+        Serial.print("Analog reading = ");
+        Serial.print(fsrReading);
+     
+        if (fsrReading < 10) {
+          Serial.println(" - No pressure");
+        } else if (fsrReading < 200) {
+          Serial.println(" - Light touch");
+        } else if (fsrReading < 500) {
+          Serial.println(" - Light squeeze");
+        } else if (fsrReading < 800) {
+          Serial.println(" - Medium squeeze");
+        } else {
+          Serial.println(" - Big squeeze");
+        }
+        
+        Serial.print("X orientation: ");
+        Serial.print(event.orientation.x);
+        Serial.print("\tY orientation: ");
+        Serial.print(event.orientation.y);
+        Serial.print("\tZ orientation: ");
+        Serial.print(event.orientation.x);
+        Serial.println("");
       }
       
-      Serial.print("X: ");
-      Serial.print(X_pos);
-      Serial.print("\tY: ");
-      Serial.print(Y_pos);
-      Serial.print("\tZ: ");
-      Serial.print(Z_pos);
-      Serial.println("");
-  
       /* 1.4. Increment the datapoint counter. */
       arr_counter += 1;
       arr_string  += "," + String(arr_counter);
@@ -154,6 +163,6 @@ void saveData(String dataString, String filename){
     dataFile.close();
   }
   else{
-    Serial.println("Error writing to file!");
+    Serial.println("Error writing to file.");
   }
 }

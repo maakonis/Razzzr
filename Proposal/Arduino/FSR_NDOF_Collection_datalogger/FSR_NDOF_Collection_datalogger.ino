40631eb3eec7d8e7f9a2d53a485682bfa88ea508
Jerome Wynne <J.J.Wynne47@gmail.com>
2017-08-05 09:26:28 +0100

    Further tidying up of central script

--- Proposal/Arduino/FSR_NDOF_Collection_datalogger/FSR_NDOF_Collection_datalogger.ino
+++ Proposal/Arduino/FSR_NDOF_Collection_datalogger/FSR_NDOF_Collection_datalogger.ino
@@ -9,31 +9,33 @@
 
 /* > General Declarations < */
 const bool PRINT_MEASUREMENTS = false;
-const bool WRITE_DEMO_FILE    = false;
-int time_step             = 0;
+int        time_step          = 0;
 
 /* > Datalogger Declarations < */
-const String HEADER = "time,th_x,th_y,th_z,acc_x,acc_y,acc_z,mag_x,mag_y,mag_z,grav_x,grav_y,grav_z,force,temperature,calibration";
-String CSV_name;
-const int CSpin = 4;
-File dataFile;
-File root;
-int file_index = 0;
+const String HEADER     = "time,th_x,th_y,th_z,acc_x,acc_y,acc_z,mag_x,mag_y,mag_z,grav_x,grav_y,grav_z,force,temperature,calibration";
+const bool   WRITE_DEMO_FILE    = false;
+const int    CS_PIN     = 4;
+int          file_index = 0;
+String       CSV_name;
+File         dataFile;
+File         root;
 
 /* > Orientation Sensor Declarations < */
-Adafruit_BNO055 bno = Adafruit_BNO055(55); // What does the 55 do?
-imu::Vector<3> orientation;
-imu::Vector<3> acceleration;
+Adafruit_BNO055 bno = Adafruit_BNO055(55);   // What does the 55 do?
+int            *calibration          = NULL; // Pointer to an array of calibration status values.
+const int      ACCELERATION_SF       = 1000; // Retains precision of acceleration floats when casting to int.
+const int      CALIBRATION_THRESHOLD = 30;   // 200 * 30 = 6000ms -> Continuous length of time the sensor must be calibrated for before data collection begins.
+const int      SAMPLING_PERIOD       = 10;   // ms
 imu::Vector<3> magnetic_field;
+imu::Vector<3> acceleration;
+imu::Vector<3> orientation;
 imu::Vector<3> gravity;
-const int ACCELERATION_SF = 100;
-int temperature;
-int *calibration = NULL;
+int            temperature;
 
 /* > Force Sensor Declarations < */
-int fsrPin = 0;     // the FSR and 10K pulldown are connected to a0
-int fsrReading;
-int force;
+const int FSRPIN      = 0;     // the FSR and 10K pulldown are connected to a0
+int       force_reading;
+int       force;
 
 
 void setup(void) {
@@ -41,9 +43,9 @@ void setup(void) {
  
   /* 1. Initialize the SD card. */
   Serial.print("Initializing SD card... ");
-  pinMode(CSpin, OUTPUT);
- 
-  if (!SD.begin(CSpin)) {
+  
+  pinMode(CS_PIN, OUTPUT);
+  if (!SD.begin(CS_PIN)) {
     Serial.println("Card failed, or not present.");
     return;
   }
@@ -55,9 +57,11 @@ void setup(void) {
   while(root.openNextFile()) {
     file_index += 1;
   }
-  CSV_name = String(file_index) + ".CSV";
+  CSV_name = file_index + ".CSV";
   
   dataFile = SD.open(CSV_name, FILE_WRITE);
+  dataFile.println(HEADER);
+
 
   /* 2. Initialize the orientation sensor. */
   Serial.print("Initializing orientation sensor... ");
@@ -65,26 +69,26 @@ void setup(void) {
     Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
     while(1);
   }
-  bno.setExtCrystalUse(true); // Synchronize the collection of data using the Feather's clock, not the one on-board the BNO.
+  bno.setExtCrystalUse(true);                         // Synchronize the collection of data using the Feather's clock, not the one on-board the BNO.
   Serial.println("Sensor initialized.");
 
+  /* 3. Calibrate the orientation sensor. */
   Serial.println("Calibrating... ");
-  while(calibration[0] != 3) {
-    // Feather's LED blinks during calibration
-    digitalWrite(13, LOW);
+  while(calibration_score < CALIBRATION_THRESHOLD) {  // Requires at least CALIBRATION_THRESHOLD successive (calibration == 3) events
+    digitalWrite(13, LOW);                            // Feather's LED blinks during calibration
     delay(100);
     digitalWrite(13, HIGH);
     delay(100);
-    calibration = getCalibrationStatus(bno); // Returns a pointer to the first element of an int[4] array
-    Serial.println("Calibration status: " + String(*(calibration)) + "\t" +
-                                            String(*(calibration+1)) + "\t" +
-                                            String(*(calibration+2)) + "\t" +
-                                            String(*(calibration+3)));
+    calibration = getCalibrationStatus(bno);          // Returns a pointer to the first element of an int[4] array
+    Serial.println("Calibration status:\t" + *(calibration)  + "\t" +
+                                             *(calibration+1) + "\t" +
+                                             *(calibration+2) + "\t" +
+                                             *(calibration+3));
+    calibration_score = max(*calibration - 2, 0)*(*calibration - 2); // Accumulator
   }
   Serial.println("Sensor calibrated.");
 
-  /* 3. Write a header to the SD card file. */
-  dataFile.println(HEADER);
+  delay(1000);
 }
 
 
@@ -101,12 +105,12 @@ void loop() {
   calibration    = getCalibrationStatus(bno);
   
   /* 2. Collect force data. */
-  fsrReading  = analogRead(fsrPin);
-  force       = map(fsrReading, 0, 1023, 0, 100);
+  force_reading  = analogRead(fsrPin);
+  force          = map(fsrReading, 0, 1023, 0, 100);
   
   /* 3. Write measurements to the SD card. */
   writeData(dataFile,
-            16,
+            16, // Number of further arguments
             (int) time_step,
             (int) orientation.x(),
             (int) orientation.y(),
@@ -125,30 +129,30 @@ void loop() {
             (int) *calibration
             );
 
-  time_step  += 1;
-
-  if (time_step % 100 == 0){
-    // Save data periodically
+  /* 4. Periodically save the data. */
+  if (time_step % 100 == 0){ // Should really introduce a save button
     dataFile.close();
     dataFile = SD.open(CSV_name, FILE_WRITE);
     }
 
-   calibration = getCalibrationStatus(bno);
-   Serial.println("Data collection status: " + String(*(calibration)) + "\t" +
-                                               String(*(calibration+1)) + "\t" +
-                                               String(*(calibration+2)) + "\t" +
-                                               String(*(calibration+3)));
+  /* 5. Print calibration status to monitor. */ 
+  calibration = getCalibrationStatus(bno);
+  Serial.println("Data collection status:\t" + *(calibration))  + "\t" +
+                                                *(calibration+1) + "\t" +
+                                                *(calibration+2) + "\t" +
+                                                *(calibration+3));
+  time_step  += 1;
+   delay(SAMPLING_PERIOD);
 }
       
 
 void writeData(File file, int n_args, ...){
-  String valString;
+  /* Writes a variable number of integer arguments to a file in CSV format. */
   va_list argList;
   
   va_start(argList, n_args);
   for (int i = 0; i < n_args; i++) { // Print each argument to a line in the file
-    valString = String(va_arg(argList, int));
-    file.print(valString + ",");
+    file.print(va_arg(argList, int) + ",");
   }
   va_end(argList);
   
@@ -157,6 +161,7 @@ void writeData(File file, int n_args, ...){
 
 
 void writeDemoFile(){
+  /* Writes a text file to verify that the SD card can be written to. */
   Serial.print("Writing demo file to card... ");
   File dataFile = SD.open("DEMOFILE.txt", FILE_WRITE);
   
@@ -166,18 +171,20 @@ void writeDemoFile(){
     Serial.println("Demo write successful.");
   }
   else {
-    Serial.print("Demo write failed..");
+    Serial.print("Demo write failed.");
   }
 }
 
+
 int * getCalibrationStatus(Adafruit_BNO055 bno){
+    /* Returns a pointer to an array of calibration statuses {sys, gyro, accel, mag}. */
     static int calibration_status[4]; // Only run once (regardless of no. of function calls)
-    uint8_t system, gyro, accel, mag;
+    static uint8_t system, gyro, accel, mag;
     system = gyro = accel = mag = 0;
     bno.getCalibration(&system, &gyro, &accel, &mag);
     calibration_status[0] = (int) system;
     calibration_status[1] = (int) gyro;
     calibration_status[2] = (int) accel;
     calibration_status[3] = (int) mag;
-    return calibration_status; // Pointer to first element of calibration_status!
+    return calibration_status;
 }
